cdd851bd63c852d61f17e784c44cdabb48b15edb
Guilherme Schroeder <guilherme.schroeder@glssolutions.us>
2026-04-05 15:11:42 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Tunez
+
+Tunez is a small demo app used to showcase how to build a music library/player using:
+
+* Elixir
+* Ash Framework
+* Sometimes Phoenix Framework for the web layer
+
+It's more of a learning/reference project to play with Ash Framework and see how it works in practice.
+
+## Setup
+
+The versions of Elixir and Erlang we're using are specified in the `.tool-versions` file. Tunez should work with any reasonably recent versions, but newer is better!
+
+* If you're using `asdf` to manage installed versions of languages, run `asdf install` to install them.
+* If you're using `mise` to manage installed versions of languages, run `mise install` to install them.
+
+Once you have those installed:
+
+* Run `mix setup` to install and setup Elixir dependencies
+* Start Phoenix endpoint with `mix phx.server` or inside IEx with `iex -S mix phx.server`
+
+Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
---
+++ lib/tunez_web/live/artists/form_live.ex
@@ -0,0 +1,92 @@
+defmodule TunezWeb.Artists.FormLive do
+  use TunezWeb, :live_view
+
+  def mount(%{"id" => artist_id}, _session, socket) do
+    artist = Tunez.Music.get_artist_by_id!(artist_id)
+    form = Tunez.Music.form_to_update_artist(artist)
+
+    socket =
+      socket
+      |> assign(:form, to_form(form))
+      |> assign(:page_title, "Edit Artist")
+      |> assign(:action_type, :update)
+
+    {:ok, socket}
+  end
+
+  def mount(_params, _session, socket) do
+    form = Tunez.Music.form_to_create_artist()
+
+    socket =
+      socket
+      |> assign(:form, to_form(form))
+      |> assign(:page_title, "New Artist")
+      |> assign(:action_type, :create)
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <Layouts.app {assigns}>
+      <.header>
+        <.h1>{@page_title}</.h1>
+      </.header>
+
+      <.simple_form
+        :let={form}
+        id="artist_form"
+        as={:form}
+        for={@form}
+        phx-change="validate"
+        phx-submit="save"
+      >
+        <.input field={form[:name]} label="Name" />
+        <.input field={form[:biography]} type="textarea" label="Biography" />
+        <:actions>
+          <.button>Save</.button>
+        </:actions>
+      </.simple_form>
+    </Layouts.app>
+    """
+  end
+
+  def handle_event("validate", %{"form" => form_data}, socket) do
+    socket =
+      update(socket, :form, fn form ->
+        AshPhoenix.Form.validate(form, form_data)
+      end)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("save", %{"form" => form_data}, socket) do
+    case AshPhoenix.Form.submit(socket.assigns.form, params: form_data) do
+      {:ok, artist} ->
+        message =
+          if socket.assigns.action_type == :create,
+            do: "Artist created successfully",
+            else: "Artist updated successfully"
+
+        socket =
+          socket
+          |> put_flash(:info, message)
+          |> push_navigate(to: ~p"/artists/#{artist}")
+
+        {:noreply, socket}
+
+      {:error, form} ->
+        message =
+          if socket.assigns.action_type == :create,
+            do: "Artist could not be created",
+            else: "Artist could not be updated"
+
+        socket =
+          socket
+          |> put_flash(:error, message)
+          |> assign(:form, form)
+
+        {:noreply, socket}
+    end
+  end
+end
